67dbea4be64192409e741b0a04115d4d0d049e1c
ppfeiffer <admin@pfeiffer-privat.de>
2024-10-21 11:06:33 +0200

    Update 2024-10-21 11:06:33

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
-const express = require("express");
-const morgan = require("morgan");
-const mysql = require("mysql");
-const myConnection = require("express-myconnection");
-const path = require('path');
-const app = express();
-require('dotenv').config();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// To get the database password from the .env file
-const PASS = process.env.DATABASE_PASSWORD;
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-// Importing routes
-const customerRoutes = require('./routes/customer');
-const callsRoutes = require('./routes/calls');
+var app = express();
 
-// settings
-app.set('port', process.env.PORT || 8001);
-app.set('view engine', 'ejs');
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-// middlewares
-app.use(morgan('dev'));
-app.use(myConnection(mysql, {
-    host: 'localhost',
-    user: 'calldb',
-    password: 'calldb',
-    port: '3306',
-    database: 'calldb'
-}, 'single'));
-app.use(express.urlencoded({extended: false}));
-
-// Routes
-app.use('/customer/', customerRoutes);
-app.use('/calls/', callsRoutes);
-
-// Static files
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Starting the server
-app.listen(app.get('port'), () => {
-    console.log('Server on port 8001');
-})
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
